90022f291bc9ca02707ae280113353dbfc2519e3
James <jameschen56@gmail.com>
2023-02-06 11:28:51 -0800

    setting up frontend

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Week 2:WHO-5 Well-Being Index</h2>
+    </div>
+  );
+}
+
+export default App;
