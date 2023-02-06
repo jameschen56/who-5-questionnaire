66b889a520b9f95923427113c14c231f1fb27137
James <jameschen56@gmail.com>
2023-02-06 13:27:28 -0800

    set up backend

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h2>Week 2:WHO-5 Well-Being Index</h2>
+      <h2>Week 2: WHO-5 Well-Being Index</h2>
     </div>
   );
 }
